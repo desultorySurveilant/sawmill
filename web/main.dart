a30a40240df14847eddbb7378127ed870dc5e13e
desultorySurveilant <antonio.marques@sbcglobal.net>
2020-03-04 21:09:15 -0600

    most barebones possible website done

--- web/main.dart
+++ web/main.dart
@@ -1,5 +1,21 @@
+import 'dart:convert' show jsonDecode;
 import 'dart:html';
 
-void main() {
-  querySelector('#output').text = 'Your Dart app is running.';
+void main() async{
+  final json = await HttpRequest.getString('data/logs.json').then(jsonDecode);
+
+  final output = querySelector('#output');
+  final searchBar = InputElement();
+  final searchButton = ButtonElement()..text = "search";
+  output.append(searchBar);
+  output.append(searchButton);
+  final results = DivElement();
+  output.append(results);
+
+  searchButton.onClick.listen((_){
+    results.text = json[searchBar.value].toString();
+  });
+  searchBar.onKeyPress.listen((e) {
+    if (e.keyCode == 13) results.text = json[searchBar.value].toString();
+  });
 }
